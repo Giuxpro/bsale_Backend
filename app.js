a18e0a0f3c673592b7753586a02b37e5e983c369
Giuxpro <albertogiusepp@gmail.com>
2022-11-12 20:42:22 -0300

    app hader middleware madification

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true, limit: '50mb'}));
 /* This is a middleware function that is used to allow the server to accept requests from any domain. */
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://bsale-front-giux.vercel.app/"); //giving access to the front domain it will make a request
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT");
